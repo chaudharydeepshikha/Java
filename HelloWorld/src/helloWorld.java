1289e5dc3a730fb42b102a51060cea5041a47061
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-10-02 16:44:56 +0530

    creating first java project (015)

---
+++ HelloWorld/src/helloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * @author Deepshikha chaudhary on 10/2/2017
+ */
+public class helloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello, world!");
+    }
+}
