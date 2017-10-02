02eeddc2ac19f3cc103f17131dde3a25517ccb68
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-10-02 17:31:50 +0530

    what are variables (018)

--- HelloWorld/src/helloWorld.java
+++ HelloWorld/src/helloWorld.java
@@ -4,5 +4,13 @@
 public class helloWorld {
     public static void main(String args[]){
         System.out.println("Hello, world!");
+
+        int firstNumber = (10 * 5) + (10 * 2);
+        int secondNumber = 20;
+        int thirdNumber = secondNumber * 2;
+
+        int result = firstNumber + secondNumber + thirdNumber;
+
+        System.out.println(result);
     }
 }
