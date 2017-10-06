81c320edfcc44eaa44e354c18a7e3f6697cfd03c
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-10-06 08:15:58 +0530

    Operators and operators precedence (024)

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -47,8 +47,37 @@ public class Main {
 
         int topScore = 100;
         int secondTopScore = 80;
-        if(topScore > secondTopScore && topScore <= 100){
+        if((topScore > secondTopScore) && (topScore <= 100)){
             System.out.println("yay! You've got the high score! ");
+        }
+
+        if((topScore > 100) || (secondTopScore < 100)){
+            System.out.println("One of the test is true");
+        }
+
+        boolean isCar = true;
+        boolean wasCar = isCar? true : false;
+        if(wasCar){
+            System.out.println("isCar is true");
+        }
+
+        /*Challenge
+        * 1. Create a double variable with the value 20.
+        * 2. Create a second variable of the type double with the value 80.
+        * 3. Add both numbers up and multiply by 25.
+        * 4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40.
+        * 5. Write a "if" statement that displays a message "Toatal was over the limit"
+        *   if the remainign total (#4) is equal to 20 or less. */
+
+        double firstVariable = 20D;
+        double secondVariable = 80D;
+
+        double myTotal = (firstVariable + secondVariable) * 25;
+        System.out.println("My total is = " + myTotal);
+        
+        myTotal = myTotal % 40;
+        if(myTotal <= 20){
+            System.out.println("Total was over the limit");
         }
     }
 }
