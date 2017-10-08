d502cfab247953798fdca3a8d7c50b8f2f79ef88
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-10-08 23:28:03 +0530

    method Overoading challenge(031)

--- MethodOverloading/src/com/company/Main.java
+++ MethodOverloading/src/com/company/Main.java
@@ -7,6 +7,10 @@ public class Main {
         calculateScore("Player-A", 500);
         calculateScore(200);
         calculateScore();
+
+        calFeetAndInchesToCentimeter(7,1);
+
+       calFeetAndInchesToCentimeter(157);
     }
 
     public static int calculateScore(String playerName, int score){
@@ -22,5 +26,28 @@ public class Main {
     public static int calculateScore(){
         System.out.println(" No player, No score");
         return 0;
+    }
+
+    public static double calFeetAndInchesToCentimeter(double feet, double inches){
+        if(feet >=0 && (inches >= 0 && inches <=12)){
+            double feetToInches = feet * 12;
+            double centimeters = (feetToInches + inches) *  2.54;
+            System.out.println(feet + " ft & "+ inches + " inches = "+ centimeters +" cms");
+            return centimeters;
+        } else {
+            System.out.println("Invalid data entered!");
+            return -1;
+        }
+    }
+    public static double calFeetAndInchesToCentimeter(double inches){
+        if(inches >=0 ){
+            double feet = (int) inches / 12;
+            double remainingInches = (int) inches % 12;
+            System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + "inches" );
+            return calFeetAndInchesToCentimeter(feet, remainingInches);
+        } else {
+            System.out.println("Invalid data entered!");
+            return -1;
+        }
     }
 }
