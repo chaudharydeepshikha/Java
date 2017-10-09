b2e8b66d33817e445a68ace78eb1a0ce137752a7
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-10-09 20:19:26 +0530

    prime number using for loop statement (034)

--- ForLoop/src/com/company/Main.java
+++ ForLoop/src/com/company/Main.java
@@ -16,6 +16,29 @@ public class Main {
             double result = calculateInterest(amount, i);
             System.out.println("The interst for rate "+ i +" % is : "+ result);
         }
+
+        int count =0;
+        for(int i = 10; i< 50; i++){
+            if(isPrime(i)){
+                System.out.println(i);
+                count++;
+                if(count == 3){
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean isPrime(int n){
+        if(n == 1){
+            return false;
+        }
+        for(int i = 2; i <= n/2; i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static double calculateInterest(double amount, double rate){
