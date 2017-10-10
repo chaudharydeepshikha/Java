9be8c15af857732485e5b022facd044379e09963
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-10-10 20:45:56 +0530

    classes part-1 (037)

---
+++ Classes/Classes/src/com/company/Car.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * @author Deepshikha chaudhary on 10/10/2017
+ */
+public class Car {
+
+    private int door;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+}
