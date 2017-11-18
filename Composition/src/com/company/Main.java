6497284055a900afd664ddda630f2773b671e3d6
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-11-18 19:35:12 +0530

    Composition (046)

---
+++ Composition/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Motherboard motherboard = new Motherboard("BJ-200","Asus", 4,6,"v2.44");
+
+        Monitor monitor = new Monitor("27inch Beast","Acer",27,new Resolution(2540,1440));
+
+        Case aCase = new Case("220B","Dell", "240",new Dimension(20,20,5));
+
+        PC pc = new PC(motherboard,monitor,aCase);
+        pc.getMonitor().drawPixelAt(1500,1200, "red");
+        pc.getMotherboard().loadProgram("Windows 10");
+        pc.getaCase().pressPowerButton();
+    }
+}
---
+++ Composition/src/com/company/PC.java
@@ -0,0 +1,28 @@
+package com.company;
+
+/**
+ * @author Deepshikha chaudhary on 11/18/2017
+ */
+public class PC {
+    private Motherboard motherboard;
+    private Monitor monitor;
+    private Case aCase;
+
+    public PC(Motherboard motherboard, Monitor monitor, Case aCase) {
+        this.motherboard = motherboard;
+        this.monitor = monitor;
+        this.aCase = aCase;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Case getaCase() {
+        return aCase;
+    }
+}
